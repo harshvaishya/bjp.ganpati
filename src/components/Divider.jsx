9f4f8265f1613e323efa280bb72f2a7fa02c6dc0
harshvaishya <harshvaishya26@gmail.com>
2026-09-09 02:34:24 +0530

    Delete src/components directoryold

--- src/components/Divider.jsx
+++
@@ -1,9 +0,0 @@
-export default function Divider({ className = '' }) {
-  return (
-    <div className={`motif-divider text-marigold-500 ${className}`} aria-hidden="true">
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-  );
-}
